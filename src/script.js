2401948dc601d71ed964aca2adb03ee855109abb
Tasi <tasi87@gmail.com>
2026-03-16 11:14:16 +0100

    type coerction

--- src/script.js
+++ src/script.js
@@ -1,2 +1,19 @@
-// Type coerction
-// = prevod jedného dátového typu na iný
+// Type coerction	-	prevod jedného dátového typu na iný
+
+console.log(1 == "1")  // Porovnáva hodnoty, ale JavaScript môže automaticky konvertovať typy.
+console.log(1 === "1") // Porovnáva hodnotu aj typ. //! !!!Žiadna automatická konverzia.!!!
+
+console.log(1 == true)		//	true
+console.log([[]] == false)	//	true
+
+//	https://dorey.github.io/JavaScript-Equality-Table/ 
+
+//note	pri podmienkach dochádza k type coerction
+if (0) { // false
+	console.log("test")
+}
+
+if (1) {	//	true
+	console.log("test")
+}
+
